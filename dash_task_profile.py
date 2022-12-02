108f4a1c96ea057b98efdf693466608040b80019
lshahsha <lshahsha@uwo.ca>
2022-12-02 13:09:51 -0500

    fixed the word cloud

--- dash_task_profile.py
+++ dash_task_profile.py
@@ -6,107 +6,6 @@ import numpy as np
 from util import *
 import os
 import parcel_hierarchy as ph
-<<<<<<< Updated upstream
-from dash import Dash, html, dcc, Input, Output
-import pandas as pd
-import plotly.express as px
-
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-
-app = Dash(__name__, external_stylesheets=external_stylesheets)
-
-def get_profile():
-
-    with open('data.pkl', 'rb') as f:  # Python 3: open(..., 'rb')
-        Prop, atlas, cmap, labels, info, profiles, conditions = pickle.load(f)
-
-    parcel = Prop.argmax(axis=0)+1
-
-
-    ax = plot_data_flat(parcel,atlas,cmap = cmap,
-                        dtype='label',
-                        labels=labels,
-                        render='plotly')
-    ax.show()
-
-    # for each parcel, get the highest scoring task
-    datasets = info.datasets.strip("'[").strip("]'").split("' '")
-    # Collect parcel profile for each task
-    label_profile = {}
-    n_highest = 3
-    for l,label in enumerate(labels):
-        if l != 0:
-            parcel_no = labels.tolist().index(label)-1
-            profile = ph.show_parcel_profile(parcel_no, profiles, conditions, datasets, show_ds='all', ncond=1, print=False)
-            highest_conditions = ['{}:{}'.format(datasets[p][:2], ' & '.join(prof[:n_highest])) for p,prof in enumerate(profile)]
-            label_profile[label]=highest_conditions
-
-    labels_alpha = sorted(label_profile.keys())
-
-    return labels_alpha
-
-# app.layout = html.Div([
-#     html.Div([
-
-#         html.Div([
-#             dcc.Dropdown(
-#                 df['Indicator Name'].unique(),
-#                 'Fertility rate, total (births per woman)',
-#                 id='crossfilter-xaxis-column',
-#             ),
-#             dcc.RadioItems(
-#                 ['Linear', 'Log'],
-#                 'Linear',
-#                 id='crossfilter-xaxis-type',
-#                 labelStyle={'display': 'inline-block', 'marginTop': '5px'}
-#             )
-#         ],
-#         style={'width': '49%', 'display': 'inline-block'}),
-
-#         html.Div([
-#             dcc.Dropdown(
-#                 df['Indicator Name'].unique(),
-#                 'Life expectancy at birth, total (years)',
-#                 id='crossfilter-yaxis-column'
-#             ),
-#             dcc.RadioItems(
-#                 ['Linear', 'Log'],
-#                 'Linear',
-#                 id='crossfilter-yaxis-type',
-#                 labelStyle={'display': 'inline-block', 'marginTop': '5px'}
-#             )
-#         ], style={'width': '49%', 'float': 'right', 'display': 'inline-block'})
-#     ], style={
-#         'padding': '10px 5px'
-#     }),
-
-#     html.Div([
-#         dcc.Graph(
-#             id='crossfilter-indicator-scatter',
-#             hoverData={'points': [{'customdata': 'Japan'}]}
-#         )
-#     ], style={'width': '49%', 'display': 'inline-block', 'padding': '0 20'}),
-#     html.Div([
-#         dcc.Graph(id='x-time-series'),
-#         dcc.Graph(id='y-time-series'),
-#     ], style={'display': 'inline-block', 'width': '49%'}),
-
-#     html.Div(dcc.Slider(
-#         df['Year'].min(),
-#         df['Year'].max(),
-#         step=None,
-#         id='crossfilter-year--slider',
-#         value=df['Year'].max(),
-#         marks={str(year): str(year) for year in df['Year'].unique()}
-#     ), style={'width': '49%', 'padding': '0px 20px 20px 20px'})
-# ])
-if __name__ == '__main__':
-    labels_alpha = get_profile()
-    # Plot the parcellation
-    # Prop = np.array(model.marginal_prob())
-    
-    print("hello")
-=======
 from wordcloud import WordCloud
 import parcel_hierarchy as ph
 import base64
@@ -247,7 +146,7 @@ app.layout = html.Div([ html.Div([
     Output(component_id='condition-1', component_property='children'),
     Output(component_id='condition-2', component_property='children'),
     Output(component_id='condition-3', component_property='children'),
-    Output(component_id = "word cloud", component_property = 'Img'),
+    # Output(component_id = "word cloud", component_property = 'Img'),
     Input(component_id='chosen_region', component_property='value'),
     Input(component_id='chosen_dataset', component_property='value'))
 
@@ -264,8 +163,8 @@ def print_conditions(region,dset):
 
     return conditions_dset[0], conditions_dset[1], conditions_dset[2]
 
-def plot_wordcloud(df, dataset = 'Mdtb', label = 'B1L'):
-    d = df.conditions[(df.dataset == 'Mdtb')& (df.label == label)]
+def plot_wordcloud(df, dset, region):
+    d = df.conditions[(df.dataset == dset)& (df.label == region)]
     wc = WordCloud (
                     background_color = 'white',
                     width = 512,
@@ -273,13 +172,18 @@ def plot_wordcloud(df, dataset = 'Mdtb', label = 'B1L'):
                         ).generate(' '.join(d))
     return wc.to_image()
 
-@app.callback(dd.Output('image_wc', 'src'), [dd.Input('image_wc', 'id')])
-def make_image(b):
+@app.callback(
+    Output('image_wc', 'src'), 
+    # Input('image_wc', 'chosen_dataset', 'chosen_region'),
+    Input(component_id='image_wc', component_property='src'),
+    Input(component_id='chosen_dataset', component_property='value'),
+    Input(component_id='chosen_region', component_property='value'),
+    )
+def make_image(b, dset, region):
     img = BytesIO()
-    plot_wordcloud(df=df).save(img, format='PNG')
+    plot_wordcloud(df, dset, region).save(img, format='PNG')
     return 'data:image/png;base64,{}'.format(base64.b64encode(img.getvalue()).decode())
 
 
 if __name__ == '__main__':
     app.run_server(debug=True)
->>>>>>> Stashed changes
